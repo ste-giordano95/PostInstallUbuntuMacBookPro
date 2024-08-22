7a273884d11c16f19c65834a6b5b1f86893203b1
Stefano Giordano <giordano.stefano1995@gmail.com>
2024-08-22 10:39:20 +0200

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -5,10 +5,18 @@ printf "//////////SCRIPT POST INSTALL UBUNTU ON MBP////////// \n";
 printf "////////////////////////BY STEWIET/////////////////// \n";
 printf "/////////////////////////////////////////////////////\n";
 
-mkdir PostInstallFile
-cd PostInstallFile
+sudo apt-get update
+
+# Crea una cartella temporanea
+TEMP_DIR=$(mktemp -d)
+echo "Directory temporanea creata: $TEMP_DIR"
+
+# Naviga nella cartella temporanea
+cd "$TEMP_DIR"
+
 printf "Installing dependencies\n";
-sudo apt install git
+sudo apt-get install -y git
+sudo apt-get install -y unzip
 printf "done\n";
 
 git clone https://github.com/patjak/bcwc_pcie.git
@@ -58,8 +66,23 @@ printf "done\n\n";
 
 cd ..
 
+# Scarica il file utilizzando wget con content-disposition
+wget --content-disposition https://cloud.mogbox.net/index.php/s/r8av1dUADSEQvXW/download
+
+# Estrai il file ZIP nella cartella temporanea
+unzip facetimehd-dkms_0.1_all-20221111.zip
+
+# Installa il pacchetto .deb
+sudo dpkg -i facetimehd-dkms_0.1_all-20221111.deb
+
+# Risolve le dipendenze mancanti
+sudo apt-get install -f
+
+# Verifica l'installazione
+dpkg -l | grep facetimehd
+
 printf "Install mbpfan\n";
-sudo apt-get install mbpfan
+sudo apt-get install -y mbpfan
 printf "done\n\n";
 
 printf "Run on start\n";
@@ -67,13 +90,17 @@ git clone https://github.com/linux-on-mac/mbpfan.git
 cd mbpfan
 sudo cp mbpfan.upstart /etc/init/mbpfan.conf
 
-cd ..
-
 sudo mbpfan
 printf "done\n\n";
 
 printf "Cleaning...\n";
-sudo rm -r PostInstallFile
+
+# Naviga fuori dalla cartella temporanea
+cd ~
+
+# Rimuove la cartella temporanea e tutto il suo contenuto
+rm -rf "$TEMP_DIR"
 printf "done\n";
+
 printf "Please Reboot system.\n";
 printf "Script Ended\n";
